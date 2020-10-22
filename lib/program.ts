3b55c6bb2dc820ac402abdbe1b6b0a96621a4ab7
dev <dev@superusers.dk>
2020-10-22 14:35:14 +0200

    feature/weather

--- lib/program.ts
+++ lib/program.ts
@@ -1,5 +1,24 @@
-const title = 'TypeScript language'
-let message:string = `<p>this is ${title} </p>`
+// npm install node-fetch
+// npm install @types/node-fetch
+
+import fetch from 'node-fetch'
+
+const address = "https://superusers-api-service-2020.azurewebsites.net/weather"
+let city      = "copenhagen"
+
+let url = `${address}/${city}`
+
+let temp:number 
 
 
-console.log(message)
+// C:\dev\vscode-nodejs-typescript-hello-world\node_modules\@types\node-fetch\index.d.ts
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data)
+        temp = data.CurrentData.temperature   
+        console.log(`The Temperatur in ${city} is today ${temp} `)
+        //The Temperatur in copenhagen is today 16
+    })
+    
+
